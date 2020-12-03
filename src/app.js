307f94aca7aa9c68861c9f49dea843c8f4ea6fd5
OscarGtzz <oscar.gutierrez.nunez@gmail.com>
2020-12-02 19:47:54 -0800

    Examen

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
+
 const mongoose = require('mongoose');
 
 const app = express();
@@ -21,7 +21,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}))
 
 app.use('/', indexRoutes);
